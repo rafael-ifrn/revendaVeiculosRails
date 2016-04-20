ea8b83b295762154fd1a120c5c63ce11bccff08d
system_source <system.source.code@gmail.com>
2016-04-20 11:27:15 -0300

    validação no campo descricao do fabricante 	não pode ser em branco 	deve ter no mínimo 2 caracteres 	apenas possua letras, número ou espaço, obedecendo a expressão regular /[a-zA-z0-9 ]+/ 	verificação na criação e atualização

--- app/models/fabricante.rb
+++ app/models/fabricante.rb
@@ -1,2 +1,7 @@
 class Fabricante < ActiveRecord::Base
+
+	validates_presence_of :descricao
+	validates_length_of :descricao, minimum: 2, allow_blank: false
+	validates_format_of :descricao, :with => /[a-zA-z0-9 ]+/, :on => [:create, :update] , :message => 'Permitido apenas letras e números'
+
 end
